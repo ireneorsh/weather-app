bdfb42369b6ee32d6da3156a751bb136f537a6aa
ireneorsh <112197977+ireneorsh@users.noreply.github.com>
2022-09-09 22:06:28 +0200

    Added a link to netlify at the bottom of the page, removed console logs

--- src/index.js
+++ src/index.js
@@ -54,14 +54,11 @@ function showCurrentCityWeather(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates.lat);
-  console.log(coordinates.lon);
   let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showForecast);
 }
 function showForecast(response) {
-  console.log(response.data.daily);
   let forecastReal = response.data.daily;
   let forecast = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
